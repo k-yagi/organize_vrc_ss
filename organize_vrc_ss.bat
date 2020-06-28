b4657a294123b92b28ec9b07e4484cdfb581e389
K::Yagi <k.yagi914@gmail.com>
2020-06-28 14:27:15 +0900

    不要業削除

--- organize_vrc_ss.bat
+++ organize_vrc_ss.bat
@@ -4,7 +4,6 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 for /f %%i in ('dir/b *.png') do (
   SET FILENAME=%%i
   SET DIRNAME=!FILENAME:~17,10!
-  echo !DIRNAME!
   if not exist !DIRNAME! mkdir !DIRNAME!
   move /-Y !FILENAME! !DIRNAME!
 )
